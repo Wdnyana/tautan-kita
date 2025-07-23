1f3d5d1a0794b2ba8b7a1fc4cb399e012ddc98ab
Wdnyana <purwawiadnyana08@gmail.com>
2025-07-23 13:52:25 +0800

    feat: display profile data and links on UI

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,14 @@
 import { type Types } from '$lib'
 
-export async function load() {
+export async function load(): Promise<Types.typesProfile.PageData> {
+	const dataProfile: Types.typesProfile.ProfileData = {
+		nama: 'Purwa',
+		username: 'prw',
+		bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+		fotoProfileUrl:
+			'https://img.freepik.com/premium-vector/default-male-user-profile-icon-vector-illustration_276184-168.jpg',
+	}
+
 	const linkButton: Types.typesProfile.DataLink[] = [
 		{
 			id: 1,
@@ -19,16 +27,8 @@ export async function load() {
 		},
 	]
 
-	const dataProfile: Types.typesProfile.ProfileData = {
-		nama: 'Purwa',
-		username: 'prw',
-		bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-		fotoProfileUrl:
-			'https://img.freepik.com/premium-vector/default-male-user-profile-icon-vector-illustration_276184-168.jpg',
-	}
-
 	return {
-		linkButton,
-		dataProfile,
+		profile: dataProfile,
+		dataLink: linkButton,
 	}
 }
